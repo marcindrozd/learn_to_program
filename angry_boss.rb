4d640b53db2e960826b4fd1665e9150cc7729c0e
Marcin Drozd <marcindrozd@me.com>
2014-12-22 22:05:07 +0100

    Completed angry boss task from chapter 5.

---
+++ angry_boss.rb
@@ -0,0 +1,8 @@
+# Write an Angry Boss program. It should rudely ask what you want. 
+# Whatever you answer, the Angry Boss should yell it back to you, 
+# and then fire you. For example, if you type in I want a raise, 
+# it should yell back WHADDAYA MEAN "I WANT A RAISE."?!?  YOU'RE FIRED!!
+
+puts "WHADDAYA WANT?!"
+response = gets.chomp
+puts "WHADDAYA MEAN \"#{response.upcase}\"?!? YOU'RE FIRED!!"
