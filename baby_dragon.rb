33c79c782c47005a473c732223e12365df548b79
Marcin Drozd <marcindrozd@me.com>
2014-12-23 20:57:42 +0100

    Completed example with baby dragon from chapter 9.

---
+++ baby_dragon.rb
@@ -0,0 +1,123 @@
+# Let's say we want to make a simple virtual pet, a baby dragon. 
+# Like most babies, it should be able to eat, sleep, and poop, 
+# which means we will need to be able to feed it, put it to bed, 
+# and take it on walks. Internally, our dragon will need to keep track of 
+# if it is hungry, tired, or needs to go, but we won't be able to see 
+# that when we interact with our dragon, just like you can't ask a human baby, 
+# "Are you hungry?". We'll also add a few other fun ways we can interact 
+# with our baby dragon, and when he is born we'll give him a name. 
+# (Whatever you pass into the  new method is passed into the initialize method for you.) 
+
+class Dragon
+	def initialize name
+		@name = name
+		@asleep = false
+		@stuffInBelly = 10 # He's full.
+		@stuffInIntestine = 0 # He doesn't need to go.
+
+		puts @name + " is born."
+	end
+
+	def feed
+		puts "You feed " + @name + "."
+		@stuffInBelly = 10
+		passageOfTime
+	end
+
+	def walk
+		puts "You walk " + @name + "."
+		@stuffInIntestine = 0
+		passageOfTime
+	end
+
+	def putToBed
+		puts "You put " + @name + " to bed."
+		@asleep = true
+		3.times do
+			if @asleep
+				passageOfTime
+			end
+			if @asleep
+				puts @name + " snores, filling the room with smoke."
+			end
+		end
+		if @asleep
+			@asleep = false
+			puts @name + " wakes up slowly."
+		end
+	end
+
+	def toss
+		puts "You toss " + @name + " up into the air."
+		puts "He giggles, which singes your eyebrows."
+		passageOfTime
+	end
+
+	def rock
+		puts "You rock " + @name + " gently."
+		@asleep = true
+		puts "He briefly dozes off..."
+		passageOfTime
+		if @asleep
+			@asleep = false
+			puts "...but wakes when you stop."
+		end
+	end
+
+	private
+
+	def hungry?
+		@stuffInBelly <= 2
+	end
+
+	def poopy?
+		@stuffInIntestine >= 8
+	end
+
+	def passageOfTime
+		if @stuffInBelly > 0
+			# Move food from belly to intestine.
+			@stuffInBelly -= 1
+			@stuffInIntestine += 1
+		else # Our dragon is starving!
+			if @asleep
+				@asleep = false
+				puts "He wakes up suddenly!"
+			end
+			puts @name + " is starving! In desperation, he ate YOU!"
+			exit # This quits the program.
+		end
+
+		if @stuffInIntestine >= 10
+			@stuffInIntestine = 0
+			puts "Whoops! " + @name + " had an accident..."
+		end
+
+		if hungry?
+			if @asleep
+				@asleep = false
+				puts "He wakes up suddenly!"
+			end
+			puts @name + "'s stomach grumbles..."
+		end
+
+		if poopy?
+			if @asleep
+				@asleep = false
+				puts "He wakes up suddenly!"
+			end
+			puts @name + " does the potty dance..."
+		end
+	end
+end
+
+pet = Dragon.new "Norbert"
+pet.feed
+pet.toss
+pet.walk
+pet.putToBed
+pet.rock
+pet.putToBed
+pet.putToBed
+pet.putToBed
+pet.putToBed
